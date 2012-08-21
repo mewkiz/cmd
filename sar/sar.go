817176ce6ef479eeec69e3df7ed57cc97d2c4db1
mewmew <random0x00@gmail.com>
2012-08-22 00:41:26 +0200

    go fmt *

--- sar/sar.go
+++ sar/sar.go
@@ -11,53 +11,53 @@ import "regexp"
 var flagInPlace bool
 
 func init() {
-   flag.Usage = usage
-   flag.BoolVar(&flagInPlace, "i", false, "edit file in place.")
-   flag.Parse()
+	flag.Usage = usage
+	flag.BoolVar(&flagInPlace, "i", false, "edit file in place.")
+	flag.Parse()
 }
 
 func usage() {
-   fmt.Fprintf(os.Stderr, "Usage: %s SEARCH REPLACE [FILE]\n", os.Args[0])
-   flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "Usage: %s SEARCH REPLACE [FILE]\n", os.Args[0])
+	flag.PrintDefaults()
 }
 
 func main() {
-   if flag.NArg() < 2 {
-      flag.Usage()
-      return
-   } else if flag.NArg() == 2 {
-      // input from: stdin
-      input, err := ioutil.ReadAll(os.Stdin)
-      if err != nil {
-         log.Fatalln(err)
-      }
-      output := sar(string(input), flag.Arg(0), flag.Arg(1))
-      fmt.Print(output)
-   } else {
-      // input from: FILE
-      input, err := ioutil.ReadFile(flag.Arg(2))
-      if err != nil {
-         log.Fatalln(err)
-      }
-      output := sar(string(input), flag.Arg(0), flag.Arg(1))
-      if flagInPlace {
-         fi, err := os.Stat(flag.Arg(2))
-         if err != nil {
-            log.Fatalln(err)
-         }
-         err = ioutil.WriteFile(flag.Arg(2), []byte(output), fi.Mode())
-         if err != nil {
-            log.Fatalln(err)
-         }
-      } else {
-         fmt.Print(output)
-      }
-   }
+	if flag.NArg() < 2 {
+		flag.Usage()
+		return
+	} else if flag.NArg() == 2 {
+		// input from: stdin
+		input, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		output := sar(string(input), flag.Arg(0), flag.Arg(1))
+		fmt.Print(output)
+	} else {
+		// input from: FILE
+		input, err := ioutil.ReadFile(flag.Arg(2))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		output := sar(string(input), flag.Arg(0), flag.Arg(1))
+		if flagInPlace {
+			fi, err := os.Stat(flag.Arg(2))
+			if err != nil {
+				log.Fatalln(err)
+			}
+			err = ioutil.WriteFile(flag.Arg(2), []byte(output), fi.Mode())
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			fmt.Print(output)
+		}
+	}
 }
 
 // sar uses regexp to search and replace provided input.
 func sar(input, search, replace string) (output string) {
-   regSearch := regexp.MustCompile(search)
-   output = regSearch.ReplaceAllString(input, replace)
-   return output
+	regSearch := regexp.MustCompile(search)
+	output = regSearch.ReplaceAllString(input, replace)
+	return output
 }
